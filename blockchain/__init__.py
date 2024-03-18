b0500da20aea5048a1d2001d6ee6538078ec044b
Jacob Thomas Redmond <127049997+Jrbiltmore@users.noreply.github.com>
2024-03-18 12:58:18 -0700

    Update __init__.py

--- blockchain/__init__.py
+++ blockchain/__init__.py
@@ -1,6 +1,13 @@
 # UUID: 072dba1e-8abb-4dfa-8c0c-fd87e825285b
 # blockchain/__init__.py
 
+"""
+This module initializes the blockchain package, making key components
+available for import elsewhere in the application. It includes the core
+blockchain mechanism, transaction management, consensus algorithms, and
+quantum-resistant security features.
+"""
+
 from .chain import Blockchain
 from .block import Block
 from .transaction import Transaction
@@ -8,3 +15,4 @@ from .quantum_security import QuantumSecurity
 from .consensus import ConsensusAlgorithm
 from .smart_contracts import SmartContract
 
+__all__ = ["Blockchain", "Block", "Transaction", "QuantumSecurity", "ConsensusAlgorithm", "SmartContract"]
